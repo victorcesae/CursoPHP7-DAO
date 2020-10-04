7e4c641d686bba7f9ce10a723be13233d1b24750
victorcesae <victor.evangelista2010@gmail.com>
2020-10-03 23:36:16 -0300

    criamos a classe Usuario

--- class/Usuario.php
+++ class/Usuario.php
@@ -1,5 +1,70 @@
 <?php
 
+require_once("config.php");
 
+class Usuario{
+    private $idusuario;
+    private $deslogin;
+    private $dessenha;
+    private $dtcadastro;
+
+    public function getIdusuario()
+    {
+        return $this->idusuario;
+    }
+    public function setIdusuario($value)
+    {
+        $this->idusuario = $value;
+    }
+    public function getDeslogin()
+    {
+        return $this->deslogin;
+    }
+    public function setDeslogin($value)
+    {
+        $this->deslogin = $value;
+    }
+    public function getDessenha()
+    {
+        return $this->dessenha;
+    }
+    public function setDessenha($value)
+    {
+        $this->dessenha = $value;
+    }
+    public function getDtcadastro()
+    {
+        return $this->dtcadastro;
+    }
+    public function setDtcadastro($value)
+    {
+        $this->dtcadastro = $value;
+    }
+
+    public function loadById($id)
+    {
+        $sql = new Sql();
+        $result = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
+            ":ID"=>$id
+        ));
+        if (count($result) > 0) {
+            $raw = $result[0];
+            $this->setIdusuario($raw['idusuario']);
+            $this->setDeslogin($raw['deslogin']);
+            $this->setDessenha($raw['dessenha']);
+            $this->setDtcadastro(new DateTime($raw['dtcadastro']));
+        }
+    }
+    public function __toString()
+    {
+        return json_encode(array(
+            "idusuario"=>$this->getIdusuario(),
+            "deslogin"=>$this->getDeslogin(),
+            "dessenha"=>$this->getDessenha(),
+            "dtusuario"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
+        ));
+    }
+
+}
 
 ?>
